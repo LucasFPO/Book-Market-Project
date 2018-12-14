800733f8e4a8e05ead726d808ba7c418915f77ac
Lucas Oliveira <lucas.f.pedrosa@gmail.com>
2018-12-14 01:10:36 -0200

    Changing cover books image, and adjusting style

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Book-Market-Project
 
-This project is about a Book Market Online where I could practice my skills in Web Development.
+This project is about a Book Market Online where I can practice my skills in Web Development.
--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -5,10 +5,10 @@ app.controller('MainController', ['$scope', function($scope) {
 
     // We used products to store an array containing two objects    
         $scope.products = [ { 
-            name: 'The Book of Trees', 
+            name: 'Enchantment', 
             price: 19, 
-            pubdate: new Date('2014', '03', '08'), 
-            cover: 'img/plant.jpg',
+            pubdate: '2011 - Guy Kawasaki', 
+            cover: 'img/Enchantment.jpg',
     
             // So far we've made a Static AngularJS. "likes" and "dislikes" will be the dynamic elements 
             // making the App more interactive
@@ -16,30 +16,33 @@ app.controller('MainController', ['$scope', function($scope) {
             dislikes: 0 }, 
 
             {
-            name: 'Program or be Programmed', 
-            price: 8, 
-            pubdate: new Date('2013', '08', '01'), 
-            cover: 'img/plant.jpg',
+            name: 'How to be the perfect dutch', 
+            price: 12, 
+            pubdate: '2015 - Kathian Brands',    
+            // pubdate: new Date('2013', '08', '01'), 
+            cover: 'img/how_to_be_the_perfect_dutch.jpg',
             likes: 0,
             dislikes: 0 },
             
             {
-            name: 'Program or be Programmed', 
-            price: 8, 
-            pubdate: new Date('2013', '08', '01'), 
-            cover: 'img/plant.jpg',
+            name: 'Game of Thrones' , 
+            price: 25, 
+            pubdate: '1996 - George R. R. Martin',
+            // pubdate: new Date('2013', '08', '01'), 
+            cover: 'img/GameOfThrones.jpg',
             likes: 0,
             dislikes: 0 } ,
             
             {
-            name: 'Program or be Programmed', 
-            price: 8, 
-            pubdate: new Date('2013', '08', '01'), 
-            cover: 'img/plant.jpg',
+            name: 'The world of abstract art', 
+            price: 20,
+            pubdate: '2017 - Emily Robbins', 
+            // pubdate: new Date('2013', '08', '01'), 
+            cover: 'img/world_of_abstract_art.jpg',
             likes: 0,
             dislikes: 0 }  
             ];
-            
+
     // We could add  as many books we desire in the array. The view will update
     // as the "ng-repeat" in the index.html loops the new elements
     
